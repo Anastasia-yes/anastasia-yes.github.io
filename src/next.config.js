0fd23be7c77407d890e15b2b87a91ffeacc5c233
cristianr909090 <cristianr909090@gmail.com>
2021-01-15 23:21:57 +1000

    back to original

--- src/next.config.js
+++ src/next.config.js
@@ -1,10 +1,6 @@
-// const isProd = process.env.NODE_ENV === 'production'
-
-// module.exports = {
-//   // Use the CDN in production and localhost for development.
-//   assetPrefix: isProd ? 'https://cdn.statically.io/gh/Anastasia-yes/anastasia-yes.github.io/gh-pages/' : '',
-// }
+const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-    assetPrefix: ".",
-};
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/Anastasia-yes/anastasia-yes.github.io/gh-pages/' : '',
+}
